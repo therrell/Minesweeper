cee91b1cb219e08b216d0c6b109f1ed2aa56562b
tom <tom@wirelessprv-10-192-246-183.near.illinois.edu>
2017-12-10 14:57:38 -0600

    added some stuff

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
 
 public class Minesweeper {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int size = 10;
+		int perc = 30;
+		Board board = new Board(size, perc);
+		Scanner sc = new Scanner(System.in);
+		
+		board.generate();
+		System.out.println("Enter the row, column, and move you would like to make separated by a space");
+		System.out.println("Possible moves: \"flag\" and \"check\"");
+		
+		while (!(board.checkLose() || board.checkWin())) {
+			String[] move = sc.nextLine().split(" ");
+			int cRow = Integer.parseInt(move[0]);
+			int cCol = Integer.parseInt(move[1]);
+
+			
+		}
+		
 
 	}
 
